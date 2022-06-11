f13ecb7fa23d388c4f7493883a560dc30f7b7a8d
Steven Moody <smoody07@gmail.com>
2022-06-11 10:47:55 -0700

    Complete sprint one

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,43 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.front = -1;
+    this.rear = -1;
+
+  }
+
+  enqueue(value) {
+    if (this.front === -1) {
+      this.front++;
+    }
+
+    this.rear++;
+    let pointer = this.rear;
+    this.storage[pointer] = value;
+
+  }
+
+  dequeue() {
+    var pointer = this.front;
+    var result = this.storage[pointer];
+    delete this.storage[pointer];
+
+    if(this.front === this.rear) {
+      this.front = -1;
+      this.rear = -1;
+    } else {
+      this.front++;
+    }
+    return result;
+  }
+
+  size() {
+    if (this.rear === -1 || this.front === -1) {
+      return 0;
+    } else {
+      return this.rear - this.front + 1;
+    }
   }
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,30 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.next = 0;
+  }
+
+  push(value) {
+    let next = this.next;
+    this.storage[next] = value;
+    this.next++;
+
+  }
+
+  pop() {
+    let next = this.next;
+    if (next > 0) {
+      var last = next -1;
+      var result = this.storage[last];
+      delete this.storage[last];
+      this.next--;
+      return result;
+    }
+  }
+
+  size() {
+    return this.next;
   }
 
 }
