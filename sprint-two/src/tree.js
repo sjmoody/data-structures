5f765a5751bc4b87dad050eb914df7f6743c9c4f
Yunsup Jung <yuun@Yunsups-MBP.attlocal.net>
2022-06-15 20:36:31 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,12 +3,11 @@
 
 // Write in functional with shared methods style
 var Tree = function(value) {
-  var newTree = {};
+  var newTree = Object.create(treeMethods);
   newTree.value = value;
-  _.extend(newTree, treeMethods); // style to extend
   // your code here
   // children is an array containing a number of subtrees
-  newTree.children = null;  // fix me
+  newTree.children = [];  // fix me
 
   return newTree;
 };
@@ -17,16 +16,26 @@ var treeMethods = {};
 
 //  .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
 treeMethods.addChild = function(value) {
-
+  var newNode = new Tree(value);
+    this.children.push(newNode);
 };
 
 // A .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
 treeMethods.contains = function(target) {
   // recursively search through tree to find target
+  if(this.value === target) {
+    return true;
+  }
+  for(ch of this.children) {
+    if(ch.contains(target)) {
+      return true;
+    }
+  }
+  return false;
 };
-
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * A: addChild is O(1), and contains is O(n)
  */
