be6f55e8c7eb53844fa520906529388f83b5921f
Steven Moody <smoody07@gmail.com>
2022-06-15 20:04:41 -0700

    Complete Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,13 +3,42 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
+
+
   list.addToTail = function(value) {
+    var newNode = new Node(value);  // create new node, which contains val and next
+    if(this.head === null) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   };
 
   list.removeHead = function() {
+    if(this.head === null) {
+      return this.head;
+    }
+    var oldHead = this.head;
+    this.head = this.head.next;
+    return oldHead.value;
   };
 
   list.contains = function(target) {
+    if (this.tail.value === target) {
+      return true;
+    }
+    // create new pointer
+    var cur = this.head;
+
+    // iterate from head to tail
+    while (cur.next !== null) {
+      if (cur.value === target) {
+        return true;
+      }
+      cur = cur.next;
+    }
+    return false;
   };
 
   return list;
--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,3 +1,7 @@
+//A set class, in prototypal style, with the following properties:
+// set only handles string values until advanced section
+
+
 var Set = function() {
   var set = Object.create(setPrototype);
   set._storage = null; // fix me
@@ -7,12 +11,16 @@ var Set = function() {
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  // An .add() method, takes any string and adds it to the set
+
 };
 
 setPrototype.contains = function(item) {
+  //A .contains() method, takes any string and returns a boolean reflecting whether it can be found in the set
 };
 
 setPrototype.remove = function(item) {
+  // A .remove() method, takes any string and removes it from the set, if present
 };
 
 /*
